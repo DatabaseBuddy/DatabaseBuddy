9d0b1418980efb8a02f5d804c1ab4a13600f64e8
timi992000 <timo992000+git@gmail.com>
2021-09-06 00:21:38 +0200

    Check for correct Entries removed

--- DatabaseBuddy.Test/Core/Extender/TestOfListExtender.cs
+++ DatabaseBuddy.Test/Core/Extender/TestOfListExtender.cs
@@ -15,47 +15,55 @@ namespace DatabaseBuddy.Test.Core.Extender
     [TestMethod]
     public void TestOfRemoveFrom_0()
     {
-      List<object> TestList = new List<object>();
+      List<int> TestList = new List<int>();
       for (int i = 0; i < 100; i++)
-        TestList.Add(new object());
+        TestList.Add(i);
       TestList.RemoveFrom(0);
-      Assert.AreEqual(0, TestList.Count);   
+      Assert.AreEqual(0, TestList.Count);
     }
     [TestMethod]
     public void TestOfRemoveFrom_10()
     {
-      List<object> TestList = new List<object>();
+      List<int> TestList = new List<int>();
       for (int i = 0; i < 100; i++)
-        TestList.Add(new object());
+        TestList.Add(i);
       TestList.RemoveFrom(10);
       Assert.AreEqual(10, TestList.Count);
+      Assert.IsTrue(TestList.Min() == 0);
+      Assert.IsTrue(TestList.Max() == 9);
     }
     [TestMethod]
     public void TestOfRemoveFrom_50()
     {
-      List<object> TestList = new List<object>();
+      List<int> TestList = new List<int>();
       for (int i = 0; i < 100; i++)
-        TestList.Add(new object());
+        TestList.Add(i);
       TestList.RemoveFrom(50);
       Assert.AreEqual(50, TestList.Count);
+      Assert.IsTrue(TestList.Min() == 0);
+      Assert.IsTrue(TestList.Max() == 49);
     }
     [TestMethod]
     public void TestOfRemoveFrom_100()
     {
-      List<object> TestList = new List<object>();
+      List<int> TestList = new List<int>();
       for (int i = 0; i < 100; i++)
-        TestList.Add(new object());
+        TestList.Add(i);
       TestList.RemoveFrom(100);
       Assert.AreEqual(100, TestList.Count);
+      Assert.IsTrue(TestList.Min() == 0);
+      Assert.IsTrue(TestList.Max() == 99);
     }
     [TestMethod]
     public void TestOfRemoveFrom_NegativeOne()
     {
-      List<object> TestList = new List<object>();
+      List<int> TestList = new List<int>();
       for (int i = 0; i < 100; i++)
-        TestList.Add(new object());
+        TestList.Add(i);
       TestList.RemoveFrom(-1);
       Assert.AreEqual(100, TestList.Count);
+      Assert.IsTrue(TestList.Min() == 0);
+      Assert.IsTrue(TestList.Max() == 99);
     }
     #endregion
   }
